411826b3fdcc51fb8b5bda1551609481feb1e9bb
JOMAR NACORDA <jnacorda04@gmail.com>
2023-01-02 15:37:34 +1300

    work pls

--- handWritten/main.py
+++ handWritten/main.py
@@ -10,9 +10,7 @@ x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 # model = tf.keras.models.Sequential()
-
-# model.add(tf.keras.layers.Flatten())
-
+# model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
 # model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
 # model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
 # model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
@@ -25,8 +23,12 @@ x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 # model.save('handWritten.model')
 
+
 model = tf.keras.models.load_model('handWritten.model')
 
 loss, acc = model.evaluate(x_test, y_test)
+
+
+
 
 
