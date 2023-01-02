24c7a19db04e315e62c17a1f6ec9d715b643d154
JOMAR NACORDA <jnacorda04@gmail.com>
2023-01-02 15:58:51 +1300

    semi worked?

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
+# mnist = tf.keras.datasets.mnist
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+# x_train = tf.keras.utils.normalize(x_train, axis=1)
+# x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 # model = tf.keras.models.Sequential()
 # model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
@@ -20,12 +20,25 @@ x_test = tf.keras.utils.normalize(x_test, axis=1)
 # model.fit(x_train, y_train, epochs=4)
 
 # model.save('handWritten.model')
-
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 model = tf.keras.models.load_model('handWritten.model')
 
-loss, acc = model.evaluate(x_test, y_test) #ignore
-
-print(loss, acc)
+_img = 1
+while os.path.isfile(f'digits/digit{_img}.png'):
+    try:
+        
+        img = cv2.imread(f'digits/digit{_img}.png')[:,:,0]
+        img = cv2.resize(img, (28, 28))
+        img = np.invert(np.array([img]))
+        prediction = model.predict(img)
+        print(f"Prediction: {np.argmax(prediction)}")
+        plt.imshow(img[0], cmap=plt.cm.binary)
+        plt.show()
+    except Exception as e:
+        print(str(e))
+    finally:
+        _img += 1
+    
 
 
 
